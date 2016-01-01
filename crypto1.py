3a9eec3d43202995cb5f4d82d5dbe2b4764ec02e
jeeberhardt <qksoneo@gmail.com>
2016-01-01 21:48:08 +0100

    add sort of tick clock to prng

--- crypto1.py
+++ crypto1.py
@@ -30,50 +30,56 @@ class Crypto1:
         assert a.bit_length() != b.bit_length(), "bit length are not equal"
         return hex(a ^ b)
 
-    def prng(self, lfsr):
+    def prng(self, lfsr, tick_clock = 1):
         """
         Apply 16-bit LFSR. 
         I am sure we can do better, but for simplicity we use string
         """
-        output = ""
 
-        for _ in range(16):
-            bit = str(int(lfsr[0],2) ^ int(lfsr[10],2) ^ int(lfsr[12],2) ^ int(lfsr[13],2) ^ int(lfsr[15],2))
+        for _ in xrange(tick_clock):
 
-            # We insert the new bit at the beginning 
-            lfsr = bit + lfsr
-            # And we take the last bit that we add to the nonce
-            output += lfsr[-1]
-            # And we remove it
-            lfsr = lfsr[0:-1]
+            output = ""
+
+            for _ in xrange(16):
+                bit = str(int(lfsr[0],2) ^ int(lfsr[10],2) ^ int(lfsr[12],2) \
+                          ^ int(lfsr[13],2) ^ int(lfsr[15],2))
+
+                # We insert the new bit at the beginning 
+                lfsr = bit + lfsr
+                # And we take the last bit that we add to the nonce
+                output += lfsr[-1]
+                # And we remove it
+                lfsr = lfsr[0:-1]
 
         return output
 
-    def initialize_nonce(self, initial_lfsr):
+    def initialize_nonce(self, initial_lfsr, tick_clock = 1):
         """ 
         Function to generate initial Nt nonce. The 16 bit LFSR is 
         iniatialized during the authentification protocol.
         The generating polynomial is x16 + x14 + x13 + x11 + 1.
         How LFSR works: https://www.youtube.com/watch?v=sKUhFpVxNWc
-        We ignore the tick clock of 9.44 us.
         """
         # Initialize 16-bit LFSR with a "random number"
         initial_lfsr = int_to_binstr(initial_lfsr)
 
         # We generate the first part of the nonce (16 bit length)
-        nonce = self.prng(initial_lfsr)
+        nonce = self.prng(initial_lfsr, tick_clock)
 
         """
-        From paper: Sinces nonces are 32 bites long and the LFSR 
-        has a 16 bit state, the first half of Nt determines 
-        the second half.
+        From paper "Dismantling Mifare Classic": Sinces nonces 
+        are 32 bits long and the LFSR has a 16 bit state, 
+        the first half of Nt determines the second half.
         """
 
         # Now we generate the second half based on the first part
-        nonce += self.prng(nonce)
+        nonce += self.prng(nonce, tick_clock)
 
         # We convert it
         self.nonce = binstr_to_int(nonce)
+
+        # Return nonce, it will be sent to the reader
+        return self.nonce
 
 class Tag(Crypto1):
     """ 
@@ -92,8 +98,10 @@ class Tag(Crypto1):
 uid = 0xc2a82df4
 key = 0xa0b1c2d3f4
 initial_lfsr = 0x104A
+tick_clock = 1
 
 card = Tag(uid, key)
-card.initialize_nonce(initial_lfsr)
+Nt = card.initialize_nonce(initial_lfsr, tick_clock)
 
+print hex(Nt)
 print card
